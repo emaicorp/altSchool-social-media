769a35fc41569eb3d4e19ba6fd9e1f6783e34ed9
wnner46 <chibuegwuchukwu548@gmail.com>
2024-08-20 16:34:52 +0100

    update on db files

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
 // Load environment variables from .env file
 require('dotenv').config();
 
-module.exports = {
+configVariables= {
   port: process.env.PORT || 5000,
   mongoURI: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
   nodeEnv: process.env.NODE_ENV || 'development',
 };
+
+modules.exports = configVariables;
